5322ff4f8e3e12fce8103dbbb383e5ce53e0f683
Nhật Nguyễn Đình <nhatnguyendinh@Nhats-MacBook-Air.local>
2023-07-18 21:43:02 +0700

    refactoring file structure

--- router/flashCard.js
+++ router/flashCard.js
@@ -5,26 +5,7 @@ const flashCardController = require('../controllers/flashCard');
 
 const flashCards = [];
 
-// router.get('/add-flashCard', (req, res, next) =>{
-//     res.render('add-flashCard', {
-//         pageTitle: 'Add flashCard',
-//         path: '/add-flashCard'
-//     })
-// })
-
 router.get('/add-flashCard', flashCardController.getAddFlashCard);
-
-// router.post('/add-flashCard', (req, res, next) =>{
-
-//     flashCards.push({
-//         title: req.body.title,
-//         imgURL: req.body.imgURL,
-//         means: req.body.means,
-//         description: req.body.description
-//     })
-//     res.redirect('/');
-// })
-
 router.post('/add-flashCard', flashCardController.postAddFlashCard);
 
 module.exports.router = router;
--- router/home.js
+++ router/home.js
@@ -1,16 +1,6 @@
 const express= require('express');
-// const flashCardData = require('./flashCard');
 const homeController = require('../controllers/home');
 const router = express.Router();
-
-// router.get('/', (req, res, next) =>{
-//     const flashCards = flashCardData.flashCars;
-//     res.render('homePage', {
-//         fls: flashCards,
-//         pageTitle: 'Home',
-//         path: '/'
-//     })
-// })
 
 router.get('/', homeController.getFlashCard);
 
